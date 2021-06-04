e62c5e9f176a9ba50a6469bad6f3f3828719f20e
GTH634 <84624144+GTH634@users.noreply.github.com>
2021-06-04 12:16:38 +0530

    Create monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -4,7 +4,21 @@
 #include <math.h>
 
 float mc_pi(int);
-
+float mc_pi(int p){
+    int count=0;
+    int total=0;
+    float pi=0;
+    for(int i=1;i<=p;i++){
+        float x=2*frandom()-1;
+        float y=2*frandom()-1;
+        total++;
+        float d=(x*x+y*y);
+        if(d<1)
+        count++;}
+        pi=4*(float)count/total;
+       
+              return pi;
+    }
 float frandom() {
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
